df63ad22b11de476bfa04cf3bd34954d0f7b0e95
Katie Cassady <katie@cassadyweb.com>
2023-12-24 07:44:31 -0500

    something

--- reviews.py
+++ reviews.py
@@ -1,2 +1,6 @@
-# add your code here
+import pandas as pd 
 
+reviews = pd.read_csv('winemag-data-130k-v2.csv', index_col=0)
+top_australia_wines = pd.DataFrame(reviews.loc[(reviews.country == 'Australia') & (reviews.points >= 95)])
+top_new_zealand_wines = pd.DataFrame(reviews.loc[(reviews.country == 'New Zealand') & (reviews.points >= 95)])
+top_oceania_wines = top_australia_wines, top_new_zealand_wines
