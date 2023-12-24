8915b27b905955f37bfbbc390f4c789eb1a56012
Katie Cassady <katie@cassadyweb.com>
2023-12-24 09:22:38 -0500

    works in jupyter but not in VSC

--- reviews.py
+++ reviews.py
@@ -1,5 +1,12 @@
 import pandas as pd 
 import csv
 
-reviews = pd.read_csv("../winemag-data-130k-v2.csv", index_col=0)
-
+with open('data\winemag-data-130k-v2.csv\winemag-data-130k-v2.csv', newline="") as csvfile:
+    reviews = csv.reader(csvfile)
+    for row in reviews:
+        print(row)
+reviews = pd.read_csv("data/winemag-data-130k-v2.csv/winemag-data-130k-v2.csv", index_col=0)
+summary_reviews = reviews.groupby('country').agg({'points': ['count', 'mean']})
+summary_reviews.columns = ['number_of_reviews', 'average_points']
+summary_reviews.reset_index(inplace=True)
+summary_reviews.to_csv('data/reviews-per-country.csv', index=False)
