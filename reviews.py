bdf93436dbc4398490bb72524293f8563393839c
Katie Cassady <katie@cassadyweb.com>
2023-12-24 08:40:22 -0500

    trying it in jupyter

--- reviews.py
+++ reviews.py
@@ -1,6 +1,5 @@
 import pandas as pd 
+import csv
 
-reviews = pd.read_csv('winemag-data-130k-v2.csv', index_col=0)
-top_australia_wines = pd.DataFrame(reviews.loc[(reviews.country == 'Australia') & (reviews.points >= 95)])
-top_new_zealand_wines = pd.DataFrame(reviews.loc[(reviews.country == 'New Zealand') & (reviews.points >= 95)])
-top_oceania_wines = top_australia_wines, top_new_zealand_wines
+reviews = pd.read_csv("../winemag-data-130k-v2.csv", index_col=0)
+
